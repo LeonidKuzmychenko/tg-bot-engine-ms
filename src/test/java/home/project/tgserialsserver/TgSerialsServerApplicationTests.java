0aaa1784d6f3fe9bc5556c5904404bc93e1dd83e
Leonid Kuzmychenko <lleonidk@ukr.net>
2021-09-30 13:18:39 +0300

    init project

---
+++ src/test/java/home/project/tgserialsserver/TgSerialsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package home.project.tgserialsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgSerialsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
