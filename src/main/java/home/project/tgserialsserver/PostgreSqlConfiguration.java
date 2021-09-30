1396001b17e3e2411a2dcf2f11996231049db135
Leonid Kuzmychenko <lleonidk@ukr.net>
2021-09-30 14:16:16 +0300

    add db connection

---
+++ src/main/java/home/project/tgserialsserver/PostgreSqlConfiguration.java
@@ -0,0 +1,80 @@
+package home.project.tgserialsserver;
+
+import org.hibernate.boot.SchemaAutoTooling;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class PostgreSqlConfiguration {
+
+    @Value("${heroku.db.postgres.url}")
+    private String url;
+
+    @Value("${heroku.db.postgres.user}")
+    private String user;
+
+    @Value("${heroku.db.postgres.password}")
+    private String password;
+
+    @Bean(name = "mainJpaDataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource driver = new DriverManagerDataSource();
+        driver.setDriverClassName("org.postgresql.Driver");
+        driver.setUrl(url);
+        driver.setUsername(user);
+        driver.setPassword(password);
+        return driver;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            @Qualifier("mainJpaDataSource") DataSource dataSource,
+            @Qualifier("mainJpaHibernateJpaVendorAdapter") HibernateJpaVendorAdapter adapter,
+            @Qualifier("mainJpaProperties") Properties properties,
+            @Qualifier("mainJpaModelPaths") String[] modelPaths) {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setPackagesToScan(modelPaths);
+        factory.setDataSource(dataSource);
+        factory.setJpaVendorAdapter(adapter);
+        factory.setJpaProperties(properties);
+        return factory;
+    }
+
+    @Bean(name = "mainJpaModelPaths")
+    public String[] jpaModelPaths() {
+        return new String[]{
+//                "com.absoft.academy.database.entities.answer.model"
+        };
+    }
+
+    @Bean(name = "mainJpaHibernateJpaVendorAdapter")
+    public HibernateJpaVendorAdapter jpaHibernateJpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setDatabase(Database.POSTGRESQL);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setShowSql(false);
+        return hibernateJpaVendorAdapter;
+    }
+
+    @Bean(name = "mainJpaProperties")
+    public Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.put(AvailableSettings.HBM2DDL_AUTO, getDDL(SchemaAutoTooling.CREATE_DROP));
+        return properties;
+    }
+
+    private String getDDL(SchemaAutoTooling schemaAutoTooling) {
+        return schemaAutoTooling.name().replace("_", "-").toLowerCase();
+    }
+
+}
